d79e75e7c6d79cbef5e0d210d9456fd969f53941
admin <nomi18401840@gmail.com>
2024-09-02 13:46:48 +0500

    Initial Commit

---
+++ Fitness App/Fitness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_AppApp.swift
+//  Fitness App
+//
+//  Created by Admin on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
